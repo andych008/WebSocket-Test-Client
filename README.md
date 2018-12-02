9847226eba13751859150817c82660537da409e4
喵叔catuncle <andych008@163.com>
2018-12-02 11:46:08 +0800

    pack crx

--- README.md
+++ README.md
@@ -4,32 +4,14 @@ show remote log. work with [log-server](https://github.com/andych008/log-server)
 
 logcat --> log-server --> log-client 
 
-## Build as desktop application
-- setup
-```
-# windows
-npm install --save-dev electron-installer-windows
-# debian
-npm install --save-dev electron-installer-debian
-```
 
-
-- run
+## run
 ```
 npm install
 npm run start
 ```
 
-- build to ./dist as desktop application
-```
-npm install
-
-# windows
-npm run build
-
-# ubuntu
-npm run build2
-```
-
-## Build as crx
-chrome --> extensions --> Pack extension
+## install in chrome
+1. 下载我已经打包好的crx  https://pan.baidu.com/s/1Rb_G6NjqfVdDzOh-z32dPA
+1. 解压log-client.crx
+2. 安装 `chrome --> extensions --> Load unpacked`
--- index.js
+++ index.js
@@ -1,16 +1,12 @@
 (function() {
     var ws = null;
     var connected = false;
-    var historyItems = [];
 
     var serverUrl;
     var connectionStatus;
-    var sendMessage;
 
-    var historyList;
     var connectButton;
     var disconnectButton;
-    var sendButton;
 
     var open = function() {
         var url = serverUrl.val();
@@ -40,8 +36,6 @@
         serverUrl.removeAttr('disabled');
         connectButton.show();
         disconnectButton.hide();
-        sendMessage.attr('disabled', 'disabled');
-        sendButton.attr('disabled', 'disabled');
     };
 
     var clearLog = function() {
@@ -53,8 +47,6 @@
         ws.send('console');
         connected = true;
         connectionStatus.text('OPENED');
-        sendMessage.removeAttr('disabled');
-        sendButton.removeAttr('disabled');
     };
 
     var onClose = function() {
@@ -80,9 +72,6 @@
 
     var addMessage = function(data, type) {
         var msg = $('<pre>').text(data);
-        if (type === 'SENT') {
-            msg.addClass('sent');
-        }
         var messages = $('#messages');
         messages.append(msg);
 
@@ -93,78 +82,16 @@
         msgBox.scrollTop = msgBox.scrollHeight;
     };
 
-    var addToHistoryList = function(item) {
-        var addedLi = $('<li>').attr('id', item.id).append(
-            $('<a>').attr('href', item.url).attr('title', item.url).attr('class', 'historyUrl').append(item.url)).append(
-            $('<span>').attr('class', 'removeHistory').append("x")).attr('style', 'display: none;').prependTo(historyList);
-
-        addedLi.toggle('slow');
-    };
-
     var loadHistory = function() {
-        historyList = $('#history');
-        historyItems = JSON.parse(localStorage.getItem('history'));
+        var url = localStorage.getItem('historyUrl');
 
-        if (!historyItems) {
-            historyItems = [];
+        if (url) {
+            serverUrl.val(url);
         }
-
-        $.each(historyItems, function(i, item) {
-            addToHistoryList(item);
-        });
-
-        if (historyItems.length>0) {
-            serverUrl.val(historyItems[historyItems.length-1].url);
-        } 
-    };
-
-    var removeHistory = function(item) {
-        var removeLi = function() {
-            $(this).remove();
-        };
-        for (var i = historyItems.length - 1; i >= 0; i--) {
-            if (historyItems[i].url === item.url && historyItems[i].msg === item.msg) {
-                var selector = 'li#' + historyItems[i].id;
-                $(selector).toggle('slow', removeLi);
-
-                historyItems.splice(i, 1);
-            }
-        }
-    };
-
-    var guid = function() {
-        function s4() {
-            return Math.floor((1 + Math.random()) * 0x10000)
-                .toString(16)
-                .substring(1);
-        }
-        return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
-            s4() + '-' + s4() + s4() + s4();
     };
 
     var saveHistory = function() {
-        var item = { 'id': guid(), 'url': serverUrl.val() };
-        for (var i = historyItems.length - 1; i >= 0; i--) {
-            if (historyItems[i].url === item.url) {
-                return;
-            }
-        }
-
-        if (historyItems.length >= 20) {
-            historyItems.shift();
-            $('#history li:last-child').remove();
-        }
-
-        historyItems.push(item);
-        localStorage.setItem('history', JSON.stringify(historyItems));
-
-        addToHistoryList(item);
-    };
-
-    var clearHistory = function() {
-        historyItems = [];
-        localStorage.clear();
-        historyList.empty();
+        localStorage.setItem('historyUrl', serverUrl.val());
     };
 
     WebSocketClient = {
@@ -172,18 +99,11 @@
             serverUrl = $('#serverUrl');
             filter = $('#filter');
             connectionStatus = $('#connectionStatus');
-            sendMessage = $('#sendMessage');
-            historyList = $('#history');
 
             connectButton = $('#connectButton');
             disconnectButton = $('#disconnectButton');
-            sendButton = $('#sendButton');
 
             loadHistory();
-
-            $('#clearHistory').click(function(e) {
-                clearHistory();
-            });
 
             connectButton.click(function(e) {
                 close();
@@ -195,46 +115,13 @@
                 close();
             });
 
-            sendButton.click(function(e) {
-                var msg = $('#sendMessage').val();
-                addMessage(msg, 'SENT');
-                ws.send(msg);
-            });
-
             $('#clearMessage').click(function(e) {
                 clearLog();
-            });
-
-            historyList.delegate('.removeHistory', 'click', function(e) {
-                var link = $(this).parent().find('a');
-                removeHistory({ 'url': link.attr('href'), 'msg': link.attr('data-msg') });
-                localStorage.setItem('history', JSON.stringify(historyItems));
-            });
-
-            historyList.delegate('.historyUrl', 'click', function(e) {
-                window.haha1 = this;
-                serverUrl.val(this.href);
-                e.preventDefault();
             });
 
             serverUrl.keydown(function(e) {
                 if (e.which === 13) {
                     connectButton.click();
-                }
-            });
-
-            var isCtrl;
-            sendMessage.keyup(function(e) {
-                if (e.which === 17) {
-                    isCtrl = false;
-                }
-            }).keydown(function(e) {
-                if (e.which === 17) {
-                    isCtrl = true;
-                }
-                if (e.which === 13 && isCtrl === true) {
-                    sendButton.click();
-                    return false;
                 }
             });
         }
